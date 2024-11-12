6efcad7e15fb78a1973f41b03792e01de7e4bd57
ncapek <capekn@centrum.cz>
2024-11-12 23:39:39 +0100

    adding train.py script along with changes to config and readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ai_engineer_interview_2024
 project repository for AI engineer case study
+
+
+List for improvements:
+- make logging configurable for each script from config.yml
+- manually checked that all target caterogies are present in all dataset folds, which might not be the case in all production cases. Should be handeled appropriately
+- target class is highly imbalanced and has high cardinality, perhaps could be grouped
--- config.py
+++ config.py
@@ -1,14 +1,5 @@
 """
 This module provides configuration management utilities for the application.
-
-It includes functionality for setting up logging, loading configurations from a YAML file,
-and managing the paths to configuration files. The configuration file is expected to be
-located in the 'config' directory, and the module will search for the file in the parent
-directory of the script.
-
-Modules:
-    - setup_logging: Configures the basic logging setup for the application.
-    - load_config: Loads configuration data from a YAML file and returns it as a dictionary.
 """
 
 import logging
@@ -17,39 +8,11 @@ from typing import Dict
 
 import yaml
 
-
-def setup_logging() -> None:
-    """
-    Set up basic logging configuration for the application.
-
-    This function configures the root logger to log messages at the INFO level or higher.
-    The log messages will include the timestamp, log level, and the message.
-    """
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(message)s"
-    )
-
-
-def load_config(config_path: str = "config.yaml") -> Dict:
+def load_config(config_path: str = "config.yml") -> Dict:
     """
     Load configuration from a YAML file located in the 'config' directory.
-
-    Args:
-        config_path (str): The relative path to the YAML configuration file to be loaded.
-                            Defaults to 'config.yaml'.
-
-    Returns:
-        Dict: A dictionary representing the loaded configuration.
-
-    Raises:
-        FileNotFoundError: If the configuration file does not exist in the expected path.
-        yaml.YAMLError: If there is an error parsing the YAML file.
-        Exception: If an unexpected error occurs while loading the configuration.
     """
-    # Dynamically get the absolute path to the config.yaml file
-    script_dir = Path(__file__).parent  # Get the directory where the script is located
-    config_file_path = script_dir.parent / 'config' / config_path  # Navigate to config directory
+    config_file_path = Path(config_path)  # Navigate to config directory
 
     # Check if the file exists before trying to open it
     if not config_file_path.exists():
